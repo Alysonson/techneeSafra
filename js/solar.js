c56b670637fd5fe85d8a21fba9d527fdcda36e79
Victor <victor.sismotto@gmail.com>
2020-09-11 22:01:58 -0300

    alterei solar e solar.js

--- js/solar.js
+++ js/solar.js
@@ -1,13 +1,47 @@
-Class Calculo {
+class Calculo {
 
-    constructor(consumo) {
-        this.consumo = consumo
+    constructor(consumo, area_telhado) {
+        this.consumo_diario = consumo/30
+        this.area_telhado = area_telhado
         this.efiencia_inversor = 0.97
         this.potencia_painel = 360
+        this.hsp = 5
+    }
+
+    numeroPainel() {
+        var quantidade = (this.consumo_diario*1000*1.2)/(this.efiencia_inversor*0.8*this.potencia_painel*this.hsp)
+        return Math.ceil(quantidade)
+    }
+
+    telhado_espaco(quantidade) {
+        var area_ocupada = quantidade * 2
+        var razao = area_ocupada / this.area_telhado
+
+        if (razao < 1) {
+            return 100
+        }
+
+        else if (razao > 1 && razao < 1.25) {
+            return 75
+        }
+
+        else if (razao > 1.25 && razao < 1.5) {
+            return 60
+        }
+
+        else {
+            return 0
+        }
     }
 }
 
-function custoProjeto() {
-    var consumo = parseFloat(document.getElementById("consumo").value)
-    console.log(consumo)
+function dimensionamento() {
+    var Consumo = parseFloat(document.getElementById("consumo").value)
+    var Area_telhado = parseFloat(document.getElementById("area_telhado").value)
+    calculo = new Calculo(Consumo, Area_telhado)
+
+    qtd_paineis = calculo.numeroPainel()
+    pct_invest = calculo.telhado_espaco(qtd_paineis)
+
+    console.log(qtd_paineis, pct_invest)
 }
